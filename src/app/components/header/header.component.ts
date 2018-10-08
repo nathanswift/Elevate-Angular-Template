af9a8453cf63ac6db26c59bbec4521a903ac72bf
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-10-08 16:20:25 -0600

    ⏣ created following icon moves with mouse stops on scroll

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import $ from 'jquery';
 import { BoundText, transformAll } from '@angular/compiler/src/render3/r3_ast';
 import { TranslationWidth } from '@angular/common';
+import { resetApplicationState } from '@angular/core/src/render3/instructions';
+import { endianness } from 'os';
 
 @Component({
   selector: 'app-header',
@@ -15,42 +17,40 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+follow() {
+  $(document).mousemove(function (e) {
+    $('.follow').offset({
+      left: e.pageX,
+      top: e.pageY + 20
+    });
+  });
+}
+
   link() {
     console.log('Link Works');
   }
 
-  fade() {
-    const $logo = $('div.col-1');
-    $logo.animateCss('fadeOut', () => {
-      if ($logo.css.display != 'none') {
-        $logo.css({
-          display: 'hidden'
-        })
-      }
-    });
-  }
 
   ngOnInit() {
-    const window_width = $(window).width() - $('#header').width();
+    this.follow();
+
+    const window_width = $(window).width() - $('.col-1').width();
 
     const document_height = $(document).height() - $(window).height();
 
     $(function () {
       $(window).scroll(function () {
-        if ($(window).scrollTop() < document_height/3) {
+        if ($(window).scrollTop() < document_height / 3) {
           const scroll_position = $(window).scrollTop();
-          const object_position_right = window_width/1.67 - window_width * (scroll_position / document_height);
-          $('#header').css({
-            'left': object_position_right
+          const header_position_right = window_width - window_width * (scroll_position / document_height);
+          $('.col-1').css({
+            display: 'flex',
+            'left': header_position_right
           });
-          $('#object').css({
-            'left': object_position_right
-          })
-        } else if ($(window).scrollTop() > document_height/3) {
-          console.log(true);
+        } else if ($(window).scrollTop() > document_height/2) {
+          console.log('middle');
         }
       });
     });
-
   }
 }
